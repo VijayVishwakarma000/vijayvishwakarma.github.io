1ca7354822d7fc008fbd26f0f0ae66d129a1a3b3
VijayVishwakarma000 <76258542+VijayVishwakarma000@users.noreply.github.com>
2021-03-19 12:56:16 +0530

    Website

--- js/app.js
+++ js/app.js
@@ -25,3 +25,19 @@ var typed = new Typed('.text', {
       nav.classList.remove('stick')
    }
   })
+
+
+
+  function send(){
+    Email.send({
+      SecureToken: "7150e9cc-8545-450e-b504-ddbc96762911",
+      To : "p.vishwa1976@gmail.com",
+      From : document.getElementById('Email').nodeValue,
+      Subject : document.getElementById('subject').nodeValue,
+      Body : document.getElementById('msg-box').nodeValue
+  }).then(
+    message => alert("messeage sended")
+    
+  );
+  }
+
